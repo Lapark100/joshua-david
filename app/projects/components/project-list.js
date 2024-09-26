4050d42bfd4fd4dc7e085e3119546a2cf16f1c11
Joshua David <parktechnologies@Joshuas-MacBook-Pro.local>
2024-09-26 07:52:30 +0100

    Project now live

--- app/projects/components/project-list.js
+++ app/projects/components/project-list.js
@@ -1,18 +1,31 @@
+'use client'
+
 import Card from "@/components/card";
 import { name, description } from "@/lib/const";
+import Button from "@/components/button";
+import { useRouter } from "next/navigation";
 export default function ProjectList() {
+
+  const email = "joshuad692@gmail.com";
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('https://budget-buddy-iota.vercel.app/login')
+  }
     
       return (
         
         <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <li className="mb-4">
-            <Card className='h-full font-sans'>
+            <Card className='h-full font-sans space-y-4'>
               <div className="flex justify-between mb-4">
               <div className="font-semibold">{name[0]}</div>
-              <div>⭐</div>
+              <div>⭐⭐⭐</div>
               </div>
             
             <div>{description[0]}</div>
+            <Button variant='default' size='sm' onClick={handleClick} className="flex justify-center items-center rounded " >View Project</Button>
             </Card>
           </li>
         
---
+++ components/button.js
@@ -0,0 +1,15 @@
+export default function Button(props) {
+    console.log(props.variants)
+const variants = {
+    default: ' bg-yellow-500  text-black'
+}
+
+const sizes = {
+    sm: 'text-sm p-3 w-28'
+}
+
+return (
+    <button {...props} className={`${props.variants? variants[props.variant] : variants['default']} ${props.sizes? sizes[props.sizes] : sizes['sm']} ${props.className}`} ></button>
+    
+)
+}
