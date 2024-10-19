d16315a19120a7861bbc47b9c7e1a9ccd83c49d7
Your Name <parktechnologies@Joshuas-MacBook-Pro.local>
2024-10-19 12:19:52 +0100

    Update to project component

--- app/projects/components/project-list.js
+++ app/projects/components/project-list.js
@@ -4,8 +4,8 @@ import Card from "@/components/card";
 import { name, description } from "@/lib/const";
 import Button from "@/components/button";
 import { useRouter } from "next/navigation";
-export default function ProjectList() {
 
+export default function ProjectList() {
   const email = "joshuad692@gmail.com";
 
   const router = useRouter()
@@ -13,24 +13,52 @@ export default function ProjectList() {
   const handleClick = () => {
     router.push('https://budget-buddy-iota.vercel.app/login')
   }
-    
-      return (
-        
-        <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <li className="mb-4">
-            <Card className='h-full font-sans space-y-4'>
-              <div className="flex justify-between mb-4">
-              <div className="font-semibold">{name[0]}</div>
-              <div>⭐⭐⭐</div>
-              </div>
-            
-            <div>{description[0]}</div>
-            <Button variant='default' size='sm' onClick={handleClick} className="flex justify-center items-center rounded " >View Project</Button>
-            </Card>
-          </li>
-        
-      </ul>
-      )
-      
-}
 
+  const handleClick1 = () => {
+    router.push('https://hr-landing-page-inky.vercel.app/')
+  }
+
+  return (
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <li className="mb-4">
+        <Card className="h-full font-sans space-y-4 flex flex-col justify-between">
+          <div className="flex justify-between mb-4">
+            <div className="font-semibold">{name[0]}</div>
+            <div>⭐⭐⭐</div>
+          </div>
+
+          <div className="flex-grow">{description[0]}</div>
+
+          <Button 
+            variant="default" 
+            size="sm" 
+            onClick={handleClick} 
+            className="mt-auto flex justify-center items-center w-full rounded"
+          >
+            View Project
+          </Button>
+        </Card>
+      </li>
+
+      <li className="mb-4">
+        <Card className="h-full font-sans space-y-4 flex flex-col justify-between">
+          <div className="flex justify-between mb-4">
+            <div className="font-semibold">{name[1]}</div>
+            <div>⭐⭐⭐</div>
+          </div>
+
+          <div className="flex-grow">{description[1]}</div>
+
+          <Button 
+            variant="default" 
+            size="sm" 
+            onClick={handleClick1} 
+            className="mt-auto flex justify-center items-center w-full rounded"
+          >
+            View Project
+          </Button>
+        </Card>
+      </li>
+    </ul>
+  );
+}
