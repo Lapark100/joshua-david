9418514c5028dd98232c613e1a39041798a5d7d1
Joshua David <joshuad692@gmail.com>
2025-01-18 15:25:56 +0100

    Update to Fourth blog post

--- app/contact/page.js
+++ app/contact/page.js
@@ -9,7 +9,7 @@ const email = "joshuad692@gmail.com";
 return (
 
 <>
-<H1> <span className="text-yellow-500"> Get in touch!</span></H1>
+<H1> <span className="text-white"> Get in touch!</span></H1>
 
 <div className="md:flex  w-full md:space-x-8 mb-8" >
                 <div className=" mb-8 md:mb-0 w-full md:w-1/2 h-40  md:h-80  overflow-hidden">
@@ -17,10 +17,10 @@ return (
                 </div>
 
                 <div className=" w-full md:w-1/2 flex flex-col justify-center  ">
-                <H1> <span className="text-yellow-500"> Have a question?</span></H1>
+                <H1> <span className="text-white"> Have a question?</span></H1>
                     <p className="leading-7 "> I&apos;m currently available for work/collaborations. Send me a mail and I&apos;ll respond in 24 hours!</p>
                     <div className=" flex  mt-4 flex-col md:block">
-                        <Link href={`mailto:${email}`} className="bg-[#cadb2b] text-[#34469b]"> Contact me</Link>
+                        <Link href={`mailto:${email}`} className="bg-[#cadb2b] text-[#34469b] text-sm p-3 w-28"> Contact me</Link>
                         
                     </div>
 
